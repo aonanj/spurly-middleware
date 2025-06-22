0d26386124f39f2f102c8de004d17dfc8d8876eb
aonanj <139290070+aonanj@users.noreply.github.com>
2025-06-21 23:36:11 -0700

    Update adapters.py

--- infrastructure/adapters.py
+++ infrastructure/adapters.py
@@ -5,7 +5,7 @@ from typing import List, Dict, Optional
 from flask import request, g
 from flask import jsonify
 
-from logger import get_logger
+from .logger import get_logger
 
 logger = get_logger(__name__)
 
